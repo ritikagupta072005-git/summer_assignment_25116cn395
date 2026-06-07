c006c936a4c8335d59ad2117fdd4765c53eee768
ritikagupta-07 <ritikagupta072005@gmail.com>
2026-06-07 13:52:56 +0530

    Update Ques-7.cpp

--- Day-2/Ques-7.cpp
+++ Day-2/Ques-7.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main(){
-int i,n,digit=0,product=1;
+int i,n,digit=0,product=1,temp;
 cout<<"Enter the number:";
 cin>>n;
+temp=n;
 while(n>0){
 digit=n%10;
 product=product*digit;
 n=n/10;
 
 }
-cout<<"product of digits "<<n<<" = "<<product<<endl;
+cout<<"product of digits "<<temp<<" = "<<product<<endl;
 return 0;
 }
