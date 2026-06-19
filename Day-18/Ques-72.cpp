1fb8d23300ddeab705bc5bba11a946a2d692c026
ritikagupta-07 <ritikagupta072005@gmail.com>
2026-06-19 20:48:33 +0530

    Update Ques-72.cpp

--- Day-18/Ques-72.cpp
+++ Day-18/Ques-72.cpp
@@ -7,23 +7,23 @@ using namespace std;
 int main() {
     int arr[100], n;
 
-    cout << "Enter number of elements: ";
-    cin >> n;
+cout << "Enter number of elements: ";
+cin >> n;
 
-    cout << "Enter array elements: ";
+cout << "Enter array elements: ";
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++) {
+ for(int i = 0; i < n - 1; i++) {
+  for(int j = 0; j < n - i - 1; j++) {
             if(arr[j] < arr[j + 1]) {   
                 swap(arr[j], arr[j + 1]);
             }
         }
     }
 
-    cout << "Descending order: ";
+ cout << "Descending order: ";
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
