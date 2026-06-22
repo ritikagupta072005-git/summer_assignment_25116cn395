499e03c1b0ffed26a37a0267af4ac2f658889d2d
ritikagupta-07 <ritikagupta072005@gmail.com>
2026-06-22 11:26:22 +0530

    Day-22 uploaded

---
+++ Day-22/Ques-87.cpp
@@ -0,0 +1,22 @@
+//Write a program to Character frequency.
+
+#include <iostream>
+using namespace std;
+int main() {
+char str[100];
+int freq[256] = {0};
+
+cout << "Enter string: ";
+cin.getline(str,100);
+
+for(int i=0; str[i]!='\0'; i++){
+        freq[str[i]]++;
+    }
+
+for(int i=0;i<256;i++){
+    if(freq[i]>0){
+cout<<char(i)<<" = "<<freq[i]<<endl;
+        }
+    }
+    return 0;
+}
